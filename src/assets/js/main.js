679f9e8416f5dbddb8f8c4fced372b16379ea595
ponzinz <simonegalaxynexus@gmail.com>
2024-04-09 18:26:21 +0200

    feat: add activeMessage on click and dynamic chat header and mesages

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,24 @@
+// Milestone 1
+//* - [✔] Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+//* - [✔] Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+// Milestone 2
+//* - [✔] Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+//* - [✔] Click sul contatto mostra la conversazione del contatto cliccato
+
+// Milestone 3
+// - Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// - Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+// Milestone 4
+// - Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
+// BONUS:
+
+// Milestone 5
+// - Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+// - Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti
+
 const { createApp } = Vue;
 
 createApp({
@@ -83,6 +104,8 @@ createApp({
                 ],
             },
             ],
+            activeChat: 0,
+            activeMessage: null
         };
     },
     methods: {
@@ -90,9 +113,22 @@ createApp({
         horizontalScroll(event) {
             event.preventDefault();
             event.currentTarget.scrollLeft += event.deltaY
+        },
+        getProfilePicture(index) {
+            const profilePicture = `./assets/img/avatar${this.contacts[index].avatar}.jpg`;
+            return profilePicture
+        },
+        showCurrentChat(clickedIndex) {
+            this.activeChat = clickedIndex;
+        },
+        printMessage(activeChat) {
+            const newMessage = {
+                date: '09/04/2024',
+                message: this.userInput
+            }
         }
     },
     mounted() {
-        // console.log('')
+        console.log(activeChat[index])
     },
 }).mount('#app');
